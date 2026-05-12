758a5dda6894b28796311ebb8f741d8795291ca7
mirsaidmirjalilov <mirsaidmirjalilov06@gmail.com>
2026-05-12 23:23:46 +0500

    DAD project

--- src/main/java/com/example/fooddeliverymarketplace/repository/RestaurantRepository.java
+++ src/main/java/com/example/fooddeliverymarketplace/repository/RestaurantRepository.java
@@ -1,7 +1,6 @@
 package com.example.fooddeliverymarketplace.repository;
 
 import com.example.fooddeliverymarketplace.entity.Restaurant;
-import com.example.fooddeliverymarketplace.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
