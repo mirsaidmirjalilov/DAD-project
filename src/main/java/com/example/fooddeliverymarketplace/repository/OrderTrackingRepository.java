dba8a2ffb8e51b84fcbf8857ee7bbdc95de68c74
mirsaidmirjalilov <mirsaidmirjalilov06@gmail.com>
2026-05-18 21:53:18 +0500

    Project Done

--- src/main/java/com/example/fooddeliverymarketplace/repository/OrderTrackingRepository.java
+++ src/main/java/com/example/fooddeliverymarketplace/repository/OrderTrackingRepository.java
@@ -2,9 +2,11 @@ package com.example.fooddeliverymarketplace.repository;
 
 import com.example.fooddeliverymarketplace.document.OrderTrackingDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface OrderTrackingRepository extends MongoRepository<OrderTrackingDocument,String> {
     Optional<OrderTrackingDocument> findByOrderId(Long orderId);
 }
